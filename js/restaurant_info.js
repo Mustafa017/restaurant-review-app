fd6029dfda50e8dc1db4b62fee466f091a4d0c2a
Mustafa Khamisi <mustafangure@gmail.com>
2018-10-23 10:28:37 +0300

    feat: Add mapbox access token

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibXVzdHkyNTQiLCJhIjoiY2puazUyMzMwMTJ0MjNra2Y3bGJoNHVreCJ9.n-Uv741L9r5lM2vXSLwWPQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
