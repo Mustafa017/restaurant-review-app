74142d15dc75a01f58c0f53909c4dfb7360a3311
Mustafa Khamisi <mustafangure@gmail.com>
2018-10-25 23:40:20 +0300

    feat: Add task to optimize images

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const imagemin = require('imagemin');
+const imageminPngquant = require('imagemin-pngquant');
 
-gulp.task('default', ()=>{
+gulp.task('optimize-images', () => {
+	imagemin(['img/*.jpg'], 'build/images', {use: [imageminPngquant()]}).then(() => {
+		console.log('Images optimized');
+	});
+})
+
+gulp.task('default',['optimize-images'], ()=>{
 	browserSync.init({
 		watch: true,
-		open: false,
 		server: './'
 	})
 })
